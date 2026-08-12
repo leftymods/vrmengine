2a192df4bd44c1f5c304f2b2b73d36f6e12e8708
leftymods <leftymods@users.noreply.github.com>
2026-08-12 17:31:23 +0300

    Add load_texture using image crate; fix borrow

--- src/model.rs
+++ src/model.rs
@@ -8,3 +8,10 @@ pub fn load_vrm(data: &[u8]) -> Result<(), anyhow::Error> {
     println!("glTF loaded: {} nodes", doc.nodes().len());
     Ok(())
 }
+
+use image::DynamicImage;
+
+pub fn load_texture(data: &[u8]) -> Result<(DynamicImage, u32, u32), anyhow::Error> {
+    let img = image::load_from_memory(data)?;
+    let w = img.width(); let h = img.height(); Ok((img, w, h))
+}
